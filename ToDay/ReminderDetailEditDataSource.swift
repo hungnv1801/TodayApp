2f928e0e1ea43f25bb438d553bd68f20da1f56c9
hung <hungnv1801@gmail.com>
2021-09-03 14:52:29 +0700

    update Reminder Detail Edit

---
+++ ToDay/ReminderDetailEditDataSource.swift
@@ -0,0 +1,108 @@
+//
+//  ReminderDetailEditDataSource.swift
+//  ToDay
+//
+//  Created by Nguyen Trung on 31/08/2021.
+//
+
+import UIKit
+
+class ReminderDetailEditDataSource: NSObject {
+    enum ReminderSection: Int, CaseIterable {
+        case title
+        case dueDate
+        case notes
+        
+        var displayText: String {
+            switch self {
+            case .title:
+                return "Title"
+            case .dueDate:
+                return "Date"
+            case .notes:
+                return "Notes"
+            }
+        }
+        
+        var numRows: Int {
+            switch self {
+            case .title, .notes:
+                return 1
+            case .dueDate:
+                return 2
+            }
+        }
+        
+        func cellIdentifier(for row: Int) -> String {
+            switch self {
+            case .title:
+                return "EditTitleCell"
+            case .dueDate:
+                return row == 0 ? "EditDateLabelCell" : "EditDateCell"
+            case .notes:
+                return "EditNotesCell"
+            }
+        }
+        
+    }
+//    Add a static computed property to return the cell identifier of the date label. The data source uses a cell identifier to dequeue cells of a specific type.
+    static var dateLabelCellIdentifier: String {
+        return ReminderSection.dueDate.cellIdentifier(for: 0)
+    }
+    
+    var reminder: Reminder
+    init(reminder: Reminder) {
+        self.reminder = reminder
+    }
+    
+    private func dequeueAndConfigureCell(for indexPath: IndexPath, from tableView: UITableView) -> UITableViewCell {
+        guard let section = ReminderSection(rawValue: indexPath.section) else {
+            fatalError("Section index out of range")
+        }
+        let identifier = section.cellIdentifier(for: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        switch section {
+        case .title:
+            if let titleCell = cell as? EditTitleCell {
+                titleCell.configure(title: reminder.title)
+            }
+        case .dueDate:
+            if indexPath.row == 0 {
+                cell.textLabel?.text = reminder.dueDate.description
+            } else {
+                if let dueDateCell = cell as? EditDateCell {
+                    dueDateCell.configure(date: reminder.dueDate)
+                }
+            }
+        case .notes:
+            if let notesCell = cell as? EditNotesCell {
+                notesCell.configure(notes: reminder.notes)
+            }
+
+        }
+        return cell
+    }
+}
+extension ReminderDetailEditDataSource: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        ReminderSection.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ReminderSection(rawValue: section)?.numRows ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        fatalError()
+        return dequeueAndConfigureCell(for: indexPath, from: tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let section = ReminderSection(rawValue: section) else {
+            fatalError("Section index out of range")
+        }
+        return section.displayText
+    }
+}
--- ToDay/ReminderDetailViewController.swift
+++ ToDay/ReminderDetailViewController.swift
@@ -8,69 +8,58 @@ import UIKit
 //
 //Creating a Detail View Controller
 class ReminderDetailViewController: UITableViewController {
-//    enum ReminderRow: Int, CaseIterable {
-//        case title
-//        case date
-//        case time
-//        case notes
-//
-//        func displayText(for reminder: Reminder?) -> String? {
-//
-//            switch self {
-//            case .title:
-//                return reminder?.title
-//            case .date:
-//                return reminder?.dueDate.description
-//            case .time:
-//                return reminder?.dueDate.description
-//            case .notes:
-//                return reminder?.notes
-//            }
-//        }
-//        //Tạo thuộc tính có tên để trả về hình ảnh cho hàng.cellImage
-//        var cellImage: UIImage? {
-//            switch self {
-//                case .title:
-//                    return nil
-//                case .date:
-//                    return UIImage(systemName: "calendar.circle")
-//                case .time:
-//                    return UIImage(systemName: "clock")
-//                case .notes:
-//                    return UIImage(systemName: "square.and.pencil")
-//            }
-//        }
-//    }
+    
     private var reminder: Reminder?
-    private var detailViewDataSource: ReminderDetailViewDataSource?
+    //    private var detailViewDataSource: ReminderDetailViewDataSource?
+    private var dataSource: UITableViewDataSource?
     func configure(with reminder: Reminder) {
         self.reminder = reminder
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setEditing(false, animated: false)
+        navigationItem.setRightBarButton(editButtonItem, animated: false)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
+
+//        guard let reminder = reminder else {
+//            fatalError("No reminder found for detail view")
+//        }
+        dataSource = ReminderDetailViewDataSource(reminder: reminder!)
+        tableView.dataSource = dataSource
+        tableView.reloadData()
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
         guard let reminder = reminder else {
             fatalError("No reminder found for detail view")
         }
-        detailViewDataSource = ReminderDetailViewDataSource(reminder: reminder)
-        tableView.dataSource = detailViewDataSource	
+        //        Add an if statement that sets the data source based on the edit mode. If editing is true, set the dataSource property to a new edit data source.
+        if editing {
+            dataSource = ReminderDetailEditDataSource(reminder: reminder)
+        } else {
+            dataSource = ReminderDetailViewDataSource(reminder: reminder)
+        }
     }
 }
 
 extension ReminderDetailViewController {
-//    static let reminderDetailCellIdentifier = "ReminderDetailCell"
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return ReminderRow.allCases.count
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderDetailCellIdentifier, for: indexPath)
-//            let row = ReminderRow(rawValue: indexPath.row)
-//            cell.textLabel?.text = row?.displayText(for: reminder)
-//            cell.imageView?.image = row?.cellImage
-//            return cell
-//    }
+    //    static let reminderDetailCellIdentifier = "ReminderDetailCell"
+    //
+    //    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        return ReminderRow.allCases.count
+    //    }
+    //
+    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //            let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderDetailCellIdentifier, for: indexPath)
+    //            let row = ReminderRow(rawValue: indexPath.row)
+    //            cell.textLabel?.text = row?.displayText(for: reminder)
+    //            cell.imageView?.image = row?.cellImage
+    //            return cell
+    //    }
 }
 
 

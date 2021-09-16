b4589474b0073eef43cf57840c27b1535c2044a2
hung <hungnv1801@gmail.com>
2021-09-16 16:33:14 +0700

    Update AddReminder

---
+++ ToDay/ViewControllers/ReminderDetailViewController.swift
@@ -0,0 +1,86 @@
+////
+////  ReminderDetailViewController.swift
+////  ToDay
+////
+////  Created by Nguyen Trung on 25/08/2021.
+////
+import UIKit
+//
+//Creating a Detail View Controller
+class ReminderDetailViewController: UITableViewController {
+    
+    typealias ReminderChangeAction = (Reminder) -> Void
+    
+    private var reminder: Reminder?
+    //    private var detailViewDataSource: ReminderDetailViewDataSource?
+    private var tempReminder: Reminder?
+    private var dataSource: UITableViewDataSource?
+    private var reminderEditAction: ReminderChangeAction?
+    private var reminderAddAction: ReminderChangeAction?
+    private var isNew = true
+    
+    
+    
+    func configure(with reminder: Reminder, isNew: Bool = true, addAction: ReminderChangeAction? = nil, editAction: ReminderChangeAction? = nil) {
+        self.reminder = reminder
+        self.isNew = isNew
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setEditing(false, animated: false)
+        navigationItem.setRightBarButton(editButtonItem, animated: false)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ReminderDetailEditDataSource.dateLabelCellIdentifier)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let navigationController = navigationController, !navigationController.isToolbarHidden {
+            navigationController.setToolbarHidden(true, animated: animated)
+        }
+    }
+    
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        guard let reminder = reminder else {
+            fatalError("No reminder found for detail view")
+        }
+        //        Add an if statement that sets the data source based on the edit mode. If editing is true, set the dataSource property to a new edit data source.
+        if editing {
+            dataSource = ReminderDetailEditDataSource(reminder: reminder) {
+                reminder in
+                self.tempReminder = reminder
+                self.editButtonItem.isEnabled = true
+            }
+            navigationItem.title = NSLocalizedString("Edit Reminder", comment: "edit reminder nav title")
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTrigger))
+        } else {
+            if let tempReminder = tempReminder {
+                self.reminder = tempReminder
+                self.tempReminder = nil
+                reminderChangeAction?(tempReminder)
+                dataSource = ReminderDetailViewDataSource(reminder: tempReminder)
+            } else {
+                dataSource = ReminderDetailViewDataSource(reminder: reminder)
+            }
+            navigationItem.title = NSLocalizedString("View Reminder", comment: "view reminder nav title")
+//            dataSource = ReminderDetailViewDataSource(reminder: reminder)
+            navigationItem.leftBarButtonItem = nil
+            editButtonItem.isEnabled = true
+        }
+        tableView.dataSource = dataSource
+        tableView.reloadData()
+    }
+    
+    @objc
+    func  cancelButtonTrigger() {
+        
+        tempReminder = nil
+        setEditing(false, animated: true)
+    }
+}
+
+
---
+++ ToDay/ViewControllers/ReminderListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  ToDay
+//
+//  Created by Nguyen Trung on 24/08/2021.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController {
+
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    static let mainStoryboardName = "Main"
+    static let detailViewControllerIdentifier = "ReminderDetailViewController"
+    
+    private var reminderListDataSource: ReminderListDataSource?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+            let destination = segue.destination as? ReminderDetailViewController,
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell) {
+            let rowIndex = indexPath.row
+            guard let reminder = reminderListDataSource?.reminder(at: rowIndex) else {
+                fatalError("Couldn't find data source for reminder list.")
+            }
+            
+//            let reminder = Reminder.testData[indexPath.row]
+            destination.configure(with: reminder){ reminder in
+                self.reminderListDataSource?.update(reminder, at: rowIndex)
+                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let navigationcontroller = navigationController,
+           navigationcontroller.isToolbarHidden {
+            navigationcontroller.setToolbarHidden(false, animated: animated)
+        }
+    }
+    
+    @IBAction func addButtonTriggered(_ sender: UIBarButtonItem) {
+        addReminder()
+    }
+    
+    private func addReminder() {
+        let storyboard = UIStoryboard(name: Self.mainStoryboardName, bundle: nil)
+        let detailViewController: ReminderDetailViewController = storyboard.instantiateViewController(identifier: Self.detailViewControllerIdentifier)
+        let reminder = Reminder(title: "New Reminder", dueDate: Date())
+        detailViewController.configure(with: reminder) { reminder in
+        }
+        let navigationController = UINavigationController(rootViewController: detailViewController)
+        present(navigationController, animated: true, completion: nil)
+        
+    }
+}
+
